86eac9c927a599cca04de5c5013a117844dfb9b6
com.Kondratov <com.Kondratov@gmail.com>
2020-10-17 13:34:30 +0300

    Craete DB context classes

---
+++ TestTask/Domain/Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestTask.Domain.Entities
+{
+    // Create customer model
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field cannot be empty")]
+        [Display(Name = "Name of сustomer")]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "Order date")]
+        public string OrderDate { get; set; }
+    }
+}
