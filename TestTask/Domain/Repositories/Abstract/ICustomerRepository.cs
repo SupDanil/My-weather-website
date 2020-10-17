7dbe0840b1a177a00b975e554cb2e0fa1c653ae1
com.Kondratov <com.Kondratov@gmail.com>
2020-10-17 13:46:15 +0300

    Create abstract repositories

---
+++ TestTask/Domain/Repositories/Abstract/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Domain.Entities;
+
+namespace TestTask.Domain.Repositories.Abstract
+{
+    interface ICustomerRepository
+    {
+        IQueryable<Customer> GetCustomerInfo();
+        Customer GetCostomerById(int id);
+        void SaveCustomerInfo(Customer entity);
+    }
+}
---
+++ TestTask/Domain/Repositories/Abstract/IDeviceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Domain.Entities;
+
+namespace TestTask.Domain.Repositories.Abstract
+{
+    interface IDeviceRepository
+    {
+        IQueryable<Device> GetDeviceInfo();
+        Device GetDeviceById(int id);
+        void SaveDeviceInfo(Device entity);
+    }
+}
