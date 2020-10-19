d002b15dd79da04e458e5deff625d7dc6bae7d7a
com.Kondratov <com.Kondratov@gmail.com>
2020-10-19 16:08:35 +0300

    Create some models

--- TestTask/Controllers/HomeController.cs
+++ TestTask/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,14 +16,21 @@ namespace TestTask.Controllers
         private readonly ILogger<HomeController> _logger;
         
 
-        public HomeController(ILogger<HomeController> logger, )
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
-           
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=820325f3e470649a5d09e482df8a8871";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using(StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = await streamReader.ReadToEndAsync();
+            }
             return View();
         }
 
